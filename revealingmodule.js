5541d5c302e8ba6b0ce8a7c19fa03fd67135363f
Jake <jake@shopifybuilder.com>
2016-05-22 22:35:35 +0100

    Committing m8

---
+++ revealingmodule.js
@@ -0,0 +1,49 @@
+var people = (function(){
+
+	//Initial Setup
+	var people = ["Trump", "Clinton"];
+
+	var $el = $('#peopleModule');
+    var $button = $el.find('button');
+    var $input = $el.find('input');
+    var $ul = $el.find('ul');
+    var template = $el.find('#people-template').html();
+
+    $button.on("click", addPerson);
+    $ul.delegate("i.del", "click", deletePerson);
+
+    _render();
+
+    function _render() {
+    	var data = {
+            people: people,
+        };
+        $ul.html(Mustache._render(template, data));
+    }
+
+	function addPerson(value) {
+		var name = (typeof value === "string") ? value : $input.val();
+		people.push(name);
+        _render();
+        $input.val('');
+	}
+
+	function deletePerson(event) {
+		var i;
+		if (typeof event === "number") {
+			i = event;
+		} else {
+			var $remove = $(event.target).closest('li');
+        	i = $ul.find('li').index($remove);
+		}
+        people.splice(i, 1);
+        _render();
+	}
+
+	//Reveal Public Methods (API)
+	return {
+		addPerson: addPerson,
+		deletePerson: deletePerson
+	}
+
+})();
